e786ff058ba171c2eb750e9262a718a88b21b943
20102181_ReactNative <daicprgl@gmail.com>
2023-09-14 23:55:55 +0700

    add 5

--- Lab01_02_JS/CodingChallenge5/index.js
+++ Lab01_02_JS/CodingChallenge5/index.js
@@ -1,45 +1,51 @@
-// CAU 1
-console.log("Thử thách viết mã số 1")
-    // data
-var data = [{
-    "Mark": {
-        chieuCao: 78,
-        khoiLuong: 1.69,
-    },
-    "John": {
-        chieuCao: 92,
-        khoiLuong: 1.95,
-    }
-}, {
-    "Mark": {
-        chieuCao: 95,
-        khoiLuong: 1.88,
-    },
-    "John": {
-        chieuCao: 85,
-        khoiLuong: 1.76,
-    }
-}]
-console.log("data: " + data.toString);
+// Coding Challenge #5
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 2
 
-// function
-function tinhBMI(chieuCao, khoiLuong) {
-    return khoiLuong / (chieuCao * chieuCao); // return khoiLuong/(chieuCao^2)
+//data
+const data = [
+  {
+    dolphins: [44, 23, 71],
+    koalas: [65, 54, 49]
+  },
+  {
+    dolphins: [85, 54, 41],
+    koalas: [23, 34, 2]
+  }
+]
+
+// Hàm tính trung bình của 3 điểm số
+const calcAverage = scores => {
+  const sum = scores.reduce((acc, cur) => acc + cur, 0)
+  return sum / scores.length
 }
 
-function tinhmarkHigherBMI(data) {
-    var Mark = tinhBMI(data.Mark.chieuCao, data.Mark.khoiLuong)
-    var John = tinhBMI(data.John.chieuCao, data.John.khoiLuong)
-    return Mark > John //  Mark có chỉ số BMI cao hơn John hay không.
+// Hàm kiểm tra người chiến thắng
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas.toFixed(3)})`)
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins.toFixed(3)})`)
+  } else {
+    console.log('No team wins')
+  }
 }
-var markHigherBMI = tinhmarkHigherBMI(data[0]);
-console.log(markHigherBMI)
-markHigherBMI = tinhmarkHigherBMI(data[1]);
-console.log(markHigherBMI)
 
-////////////////////////////////////////////////
-// CAU2
-console.log("Thử thách viết mã số 2")
-data.forEach((value) => {
-    tinhmarkHigherBMI(value) ? console.log("Chỉ số BMI của Mark cao hơn John!") : console.log("Chỉ số BMI của John cao hơn Mark!")
+// Kiểm tra từng set dữ liệu
+data.forEach((set, index) => {
+  const avgDolphins = calcAverage(set.dolphins)
+  const avgKoalas = calcAverage(set.koalas)
+  console.log(`§ Data ${index + 1}: Dolphins score ${set.dolphins.join(', ')}. Koalas score ${set.koalas.join(', ')}`)
+  checkWinner(avgDolphins, avgKoalas)
 })
