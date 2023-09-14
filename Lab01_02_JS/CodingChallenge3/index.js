c60d59df3b6e2b3e22a192893aabb8ad9670b61f
20102181_ReactNative <daicprgl@gmail.com>
2023-09-14 23:31:54 +0700

    master

--- Lab01_02_JS/CodingChallenge3/index.js
+++ Lab01_02_JS/CodingChallenge3/index.js
@@ -1,45 +1,73 @@
-// CAU 1
-console.log("Thử thách viết mã số 1")
-    // data
-var data = [{
-    "Mark": {
-        chieuCao: 78,
-        khoiLuong: 1.69,
-    },
-    "John": {
-        chieuCao: 92,
-        khoiLuong: 1.95,
-    }
-}, {
-    "Mark": {
-        chieuCao: 95,
-        khoiLuong: 1.88,
-    },
-    "John": {
-        chieuCao: 85,
-        khoiLuong: 1.76,
-    }
-}]
-console.log("data: " + data.toString);
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against eachother 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. Hint: Use a logical operator to test for minimum score, as well as multiple else-if blocks
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
-// function
-function tinhBMI(chieuCao, khoiLuong) {
-    return khoiLuong / (chieuCao * chieuCao); // return khoiLuong/(chieuCao^2)
+//data
+var data = [
+  {
+    dolphins: {
+      scores: [96, 108, 89]
+    },
+    koalas: {
+      scores: [88, 91, 110]
+    }
+  },
+  {
+    dolphins: {
+      scores: [97, 112, 101]
+    },
+    koalas: {
+      scores: [109, 95, 123]
+    }
+  },
+  {
+    dolphins: {
+      scores: [97, 112, 101]
+    },
+    koalas: {
+      scores: [109, 95, 106]
+    }
+  }
+]
+
+// Hàm tính trung bình điểm
+function calculateAverage (point) {
+  const sum = point.reduce((acc, score) => acc + score, 0)
+  return sum / point.length
 }
 
-function tinhmarkHigherBMI(data) {
-    var Mark = tinhBMI(data.Mark.chieuCao, data.Mark.khoiLuong)
-    var John = tinhBMI(data.John.chieuCao, data.John.khoiLuong)
-    return Mark > John //  Mark có chỉ số BMI cao hơn John hay không.
-}
-var markHigherBMI = tinhmarkHigherBMI(data[0]);
-console.log(markHigherBMI)
-markHigherBMI = tinhmarkHigherBMI(data[1]);
-console.log(markHigherBMI)
+// Hàm kiểm tra người chiến thắng và in kết quả ra console
+function checkWinner (dolphinsScores, koalasScores) {
+  var dolphinsAverage = calculateAverage(dolphinsScores)
+  var koalasAverage = calculateAverage(koalasScores)
 
-////////////////////////////////////////////////
-// CAU2
-console.log("Thử thách viết mã số 2")
-data.forEach((value) => {
-    tinhmarkHigherBMI(value) ? console.log("Chỉ số BMI của Mark cao hơn John!") : console.log("Chỉ số BMI của John cao hơn Mark!")
-})
+  if (dolphinsAverage >= 100 && dolphinsAverage > koalasAverage) {
+    console.log('Dolphins win!')
+  } else if (koalasAverage >= 100 && koalasAverage > dolphinsAverage) {
+    console.log('Koalas win!')
+  } else if (
+    dolphinsAverage === koalasAverage &&
+    dolphinsAverage >= 100 &&
+    koalasAverage >= 100
+  ) {
+    console.log("It's a draw!")
+  } else {
+    console.log('No one wins the trophy.')
+  }
+}
+
+// Kiểm tra từng set dữ liệu
+for (var i = 0; i < data.length; i++) {
+  var dolphinsScores = data[i].dolphins.scores
+  var koalasScores = data[i].koalas.scores
+  console.log(`+ Round ${i + 1}:`)
+  checkWinner(dolphinsScores, koalasScores)
+}
